858946b24cb38d5a9a796f365730fc4dce226f50
Ariel <128343170+tanggol69@users.noreply.github.com>
2023-10-29 21:05:57 +0800

    Update team-website2

---
+++ components/ui/footer.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="py-12 md:py-16">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
+          {/* Top area: Blocks */}
+          <div className="grid md:grid-cols-12 gap-8 lg:gap-20 mb-8 md:mb-12">
+
+            {/* 1st block */}
+            <div className="md:col-span-4 lg:col-span-5">
+              <div className="mb-2">
+                {/* Logo */}
+                <Link href="/" className="inline-block" aria-label="Cruip">
+                <div>
+    <img src="/images/logo.png" alt="Cruip Logo" width="100" />
+  </div>
+                </Link>
+              </div>
+              <div className="text-gray-400">BCD::GO is passionate about coding and dedicated to helping you succeed in the digital world.</div>
+            </div>
+
+            {/* 2nd, 3rd and 4th blocks */}
+            <div className="md:col-span-8 lg:col-span-7 grid sm:grid-cols-3 gap-8">
+
+              {/* 2nd block */}
+              <div className="text-sm">
+                <h6 className="text-gray-200 font-medium mb-1">*1st Block*</h6>
+                <ul>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                </ul>
+              </div>
+
+              {/* 3rd block */}
+              <div className="text-sm">
+                <h6 className="text-gray-200 font-medium mb-1">*2nd Block*</h6>
+                <ul>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                </ul>
+              </div>
+
+              {/* 4th block */}
+              <div className="text-sm">
+                <h6 className="text-gray-200 font-medium mb-1">*3rd Block*</h6>
+                <ul>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                  <li className="mb-1">
+                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">BCD::GO</Link>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+
+          </div>
+
+          
+
+            {/* Copyrights note */}
+            <div className="text-gray-400 text-sm mr-4">&copy; BCD::GO All rights reserved.</div>
+
+          
+
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ components/zigzag.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+import FeatImage01 from '@/public/images/features-03-image-01.png'
+import FeatImage02 from '@/public/images/features-03-image-02.png'
+import FeatImage03 from '@/public/images/features-03-image-03.png'
+
+export default function Zigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
+            <h1 className="h2 mb-4">Simple and Affordable Pricing</h1>
+            <p className="text-xl text-gray-400">We offer Basic and Premium plans that cater to different budgets and goals. Our commitment to affordability means you get exceptional value without compromising on quality. Explore our pricing options to find the plan that best suits your requirements, and remember, we stand behind our work with a satisfaction guarantee.</p>
+          </div>
+
+          
+
+          </div>
+
+        </div>
+      
+    </section>
+  )
+}
