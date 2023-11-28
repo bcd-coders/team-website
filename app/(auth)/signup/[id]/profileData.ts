0d483d6a1c1fa966740fd62d0b3d496d1f0acf33
Awesome0729 <55270553+Awesome0729@users.noreply.github.com>
2023-11-28 15:02:38 +0800

    added azaleah data

--- app/(auth)/signup/[id]/profileData.ts
+++ app/(auth)/signup/[id]/profileData.ts
@@ -1,126 +1,160 @@
 export const profileData = [
-    {
-        Name: "Lanz Montelibano",
-        Title: "Developer",
-        Email: "lanzmontelibano21@gmail.com",
-        Skills: {
-          programming: ["Go", "TypeScript", "MongoDB"],
-          soft: ["problem solving", "decision making"]
-        },
-        workExperience: [
-            {
-              title: "Software Engineer",
-              company: "Cest Louvi",
-              location: "Bacolod City",
-              date: "Aug 2023 - Present",
-              description: "Developing and maintaining API's and creating websites.",
-              bulletPoints: [
-                "created a CLI to validate database.",
-                "created a website to showcase local coffee shops in Bacolod City.",
-                "worked on API that uses GO and MongoDB.",
-              ]
-            },
-            {
-              title: "Software Developer",
-              company: "Miller Development",
-              location: "Bacolod City",
-              date: "Nov 2022 - March 2023",
-              description: "Software Developer in training.",
-              bulletPoints: [
-                "finished tasks for onboarding process.",
-                "created two projects during onboarding process.",
-              ]
-            },
-        ],
-        Education: [
-          {
-            school: "USLS",
-            location: "Bacolod City",
-            degree: "Computer Science",
-            date: "2018 - 2022"
-          }
-        ]
+  {
+    Name: "Lanz Montelibano",
+    Title: "Developer",
+    Email: "lanzmontelibano21@gmail.com",
+    Skills: {
+      programming: ["Go", "TypeScript", "MongoDB"],
+      soft: ["problem solving", "decision making"]
     },
-    {
-        Name: "Ziegfred Carmona",
-        Title: "Developer",
-        Email: "carmonaziegfred@gmail.com",
-        Skills: {
-          programming: ["C#", "GO", "Python", "TypeScript"],
-          soft: ["Adaptability", "Teamwork", "Problem Solving"]
-        },
-        workExperience: [
-            {
-              title: "Software Engineer",
-              company: "Cest Louvi",
-              location: "Bacolod City",
-              date: "Sept 2023 - Present",
-              description: "Experienced computer science graduate specializing in API development and website creation, adept at leveraging innovative technologies to deliver user-friendly solutions and possessing strong problem-solving skills.",
-              bulletPoints: [
-                "Established an informative website featuring Bacolod City's local coffee shops, offering users an interactive platform to explore and discover the vibrant coffee culture.",
-                "Collaborated on the creation of an API using GO and MongoDB, showcasing proficiency in backend technologies and database management.",
-                "Developed a Command Line Interface (CLI) dedicated to validating databases, ensuring data integrity and reliability.",
-              ]
-            },
-        ],
-        Education: [
-          {
-            school: "USLS",
-            location: "Bacolod City",
-            degree: "Computer Science",
-            date: "2018 - 2022"
-          }
+    workExperience: [
+      {
+        title: "Software Engineer",
+        company: "Cest Louvi",
+        location: "Bacolod City",
+        date: "Aug 2023 - Present",
+        description: "Developing and maintaining API's and creating websites.",
+        bulletPoints: [
+          "created a CLI to validate database.",
+          "created a website to showcase local coffee shops in Bacolod City.",
+          "worked on API that uses GO and MongoDB.",
         ]
-    },
-    {
-      Name: "Rex Posadas",
-      Title: "Technical Lead",
-      Email: "rex.posadas@gmail.com",
-      Skills: {
-        programming: ["Django/Python", "GO", "Kubernetes ","PostgreSQL","Docker","RESTful WebServices"],
-        soft: ["Experienced", "Team Leadership", "Mentor", "Project Management"]
       },
-      workExperience: [
-          {
-            title: "Freelance",
-            company: "N/A",
-            location: "Remote",
-            date: "March 2023 - Present",
-            description: "Working with small businesses to bring their ideas to market.",
-            bulletPoints: [
-              "Project Management: Techniques and tools to plan, execute, and oversee project tasks efficiently.",
-              "Go (Programming Language): A programming language known for its simplicity and efficiency, ideal for building scalable systems.",
-              "PostgreSQL: An open-source relational database management system known for its reliability and extensibility.",
-              "RESTful Web Services: Design principles for creating web services that follow Representational State Transfer (REST) architecture.",
-              "Django: A powerful web framework for building web applications using Python.",
-              "Kubernetes: A container orchestration platform that automates the deployment, scaling, and management of containerized applications.",
-            ]
-          },
-          {
-            title: "Mentor",
-            company: "Oregon State University",
-            location: "Oregon, United States",
-            date: "September 2022 - Present",
-            description: "Mentor Computer Science students in the Software Innovation Lab.",
-            bulletPoints: [
-              "Guided students in learning the Go programming language, emphasizing its simplicity and efficiency for developing robust applications.",
-              "Mentored students in utilizing MongoDB, focusing on its NoSQL structure and teaching how to store and manage data in a flexible and scalable way.",
-              "Mentored students in leveraging Node.js for server-side development, emphasizing its capabilities in building scalable and high-performance network applications.",
-              "Provided mentorship on Docker, teaching students how to use containers for packaging and deploying applications, ensuring a consistent and reproducible development environment.",
-            ]
-          },
-          {
-            title: "Head of Engineering/Founder",
-            company: "Agrarian Labs",
-            location: "Austin, Texas",
-            date: "September 2014 - Dec 2018",
-            description: "Founder of a new ag-tech company (grew company from 2 to 14 employees with offices in Phoenix, AZ and Austin, TX).",
-            bulletPoints: [
-              "Built, managed, mentored and led an engineering team.",
-              "Architected high volume IoT application that received over 10Gigs of data per day from IoT devices across the United States.",
-              "Executive team member responsible for full cycle recruiting along with strategies and organizing Technical Meetups for the purpose of competing in South by Southwest Conference and Festival - finalists in SXSW (Enterprise and Smart Data Technologies category)",
-            ]
-          },
-      ]
-  }
+      {
+        title: "Software Developer",
+        company: "Miller Development",
+        location: "Bacolod City",
+        date: "Nov 2022 - March 2023",
+        description: "Software Developer in training.",
+        bulletPoints: [
+          "finished tasks for onboarding process.",
+          "created two projects during onboarding process.",
+        ]
+      },
+    ],
+    Education: [
+      {
+        school: "USLS",
+        location: "Bacolod City",
+        degree: "Computer Science",
+        date: "2018 - 2022"
+      }
+    ]
+  },
+  {
+    Name: "Ziegfred Carmona",
+    Title: "Developer",
+    Email: "carmonaziegfred@gmail.com",
+    Skills: {
+      programming: ["C#", "GO", "Python", "TypeScript"],
+      soft: ["Adaptability", "Teamwork", "Problem Solving"]
+    },
+    workExperience: [
+      {
+        title: "Software Engineer",
+        company: "Cest Louvi",
+        location: "Bacolod City",
+        date: "Sept 2023 - Present",
+        description: "Experienced computer science graduate specializing in API development and website creation, adept at leveraging innovative technologies to deliver user-friendly solutions and possessing strong problem-solving skills.",
+        bulletPoints: [
+          "Established an informative website featuring Bacolod City's local coffee shops, offering users an interactive platform to explore and discover the vibrant coffee culture.",
+          "Collaborated on the creation of an API using GO and MongoDB, showcasing proficiency in backend technologies and database management.",
+          "Developed a Command Line Interface (CLI) dedicated to validating databases, ensuring data integrity and reliability.",
+        ]
+      },
+    ],
+    Education: [
+      {
+        school: "USLS",
+        location: "Bacolod City",
+        degree: "Computer Science",
+        date: "2018 - 2022"
+      }
+    ]
+  },
+  {
+    Name: "Rex Posadas",
+    Title: "Technical Lead",
+    Email: "rex.posadas@gmail.com",
+    Skills: {
+      programming: ["Django/Python", "GO", "Kubernetes ", "PostgreSQL", "Docker", "RESTful WebServices"],
+      soft: ["Experienced", "Team Leadership", "Mentor", "Project Management"]
+    },
+    workExperience: [
+      {
+        title: "Freelance",
+        company: "N/A",
+        location: "Remote",
+        date: "March 2023 - Present",
+        description: "Working with small businesses to bring their ideas to market.",
+        bulletPoints: [
+          "Project Management: Techniques and tools to plan, execute, and oversee project tasks efficiently.",
+          "Go (Programming Language): A programming language known for its simplicity and efficiency, ideal for building scalable systems.",
+          "PostgreSQL: An open-source relational database management system known for its reliability and extensibility.",
+          "RESTful Web Services: Design principles for creating web services that follow Representational State Transfer (REST) architecture.",
+          "Django: A powerful web framework for building web applications using Python.",
+          "Kubernetes: A container orchestration platform that automates the deployment, scaling, and management of containerized applications.",
+        ]
+      },
+      {
+        title: "Mentor",
+        company: "Oregon State University",
+        location: "Oregon, United States",
+        date: "September 2022 - Present",
+        description: "Mentor Computer Science students in the Software Innovation Lab.",
+        bulletPoints: [
+          "Guided students in learning the Go programming language, emphasizing its simplicity and efficiency for developing robust applications.",
+          "Mentored students in utilizing MongoDB, focusing on its NoSQL structure and teaching how to store and manage data in a flexible and scalable way.",
+          "Mentored students in leveraging Node.js for server-side development, emphasizing its capabilities in building scalable and high-performance network applications.",
+          "Provided mentorship on Docker, teaching students how to use containers for packaging and deploying applications, ensuring a consistent and reproducible development environment.",
+        ]
+      },
+      {
+        title: "Head of Engineering/Founder",
+        company: "Agrarian Labs",
+        location: "Austin, Texas",
+        date: "September 2014 - Dec 2018",
+        description: "Founder of a new ag-tech company (grew company from 2 to 14 employees with offices in Phoenix, AZ and Austin, TX).",
+        bulletPoints: [
+          "Built, managed, mentored and led an engineering team.",
+          "Architected high volume IoT application that received over 10Gigs of data per day from IoT devices across the United States.",
+          "Executive team member responsible for full cycle recruiting along with strategies and organizing Technical Meetups for the purpose of competing in South by Southwest Conference and Festival - finalists in SXSW (Enterprise and Smart Data Technologies category)",
+        ]
+      },
+    ]
+
+  },
+  {
+    Name: "Azaleah Karyl Caram",
+    Title: "Project Manager",
+    Email: "azaleahkaryl@gmail.com",
+    Skills: {
+      Tools: ["MS Office", "Google Workspace", "Editing"],
+      soft: ["Fast Learner", "Flexible", "Computer literate"]
+    },
+    workExperience: [
+      {
+        title: "Project Manager",
+        company: "Cest Louvi",
+        location: "Bacolod City",
+        date: "August 2023 - Present",
+        description: "Work with Ces't LouVi as Project Manager. ",
+        bulletPoints: [
+          "Manage the task list and ensures that tasks are being done efficiently.",
+          "In charge of meeting agenda",
+          "Formatting formal documents/files",
+          "Work with the team to finish tasks",
+          "Keeps the team reminded of their tasks and deadlines",
+        ]
+      },
+    ],
+    Education: [
+      {
+        school: "Riverside College, Inc.",
+        location: "Bacolod City",
+        degree: "Nursing",
+        date: "2020 - present"
+      }
+    ]
+  },
 ]
